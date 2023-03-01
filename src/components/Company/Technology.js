60867263442901c20d9a12f36161ca0bbdc61b84
Aayush-Gandhi <gandhiaayush21@gmail.com>
2023-03-01 18:42:14 +0530

    01/03-06:42

--- src/components/Company/Technology.js
+++ src/components/Company/Technology.js
@@ -8,7 +8,10 @@ export default function Technology() {
                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. since the 1500s</p>
                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
           </div>
-          <div className="flex"></div>
+          <div className="flex">
+               <img src="/Images/Contactus/Technology1.png" alt="" />
+               <img src="/Images/Contactus/Technology2.png" alt="" />
+          </div>
     </div>
   )
 }
--- src/routes/Aboutus.js
+++ src/routes/Aboutus.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { Footer, Intro, Navbar } from '../components'
+import Technology from '../components/Company/Technology'
 
 export default function Aboutus() {
   return (
     <div>
       <Navbar />
       <Intro />
+      <Technology />
       <Footer />
     </div>
   )
