0928f92874e6c10f953004165c3fcf1f40534d95
Aayush-Gandhi <126546151+Aayush-Gandhi@users.noreply.github.com>
2023-06-19 22:22:35 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,9 @@ connectDB();
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000']
+}))
 app.use("/api/v1/auth",authRoutes)
 app.use(morgan("dev"))
 app.get("/",(req,res)=>{
